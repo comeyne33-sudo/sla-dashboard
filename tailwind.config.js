53456b123f2a813b7e62faab9b13fc7054e2c847
Jouw Naam <jouw-email@voorbeeld.com>
2026-01-26 18:44:16 +0100

    Deploy fixed Tailwind version3

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        border: "hsl(var(--border))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
